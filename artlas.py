577ba400bead747c6482a6dd49a57d3cc85a2d6f
Matheus Bernardes <mthbernardes@gmail.com>
2016-06-06 22:58:43 -0300

    Add Zabbix function

--- artlas.py
+++ artlas.py
@@ -1,7 +1,7 @@
 import re,requests,json,telepot,time,apache_log_parser
 from pygtail import Pygtail
-from pyzabbix import ZabbixAPI
 from threading import Thread
+from pyzabbix import ZabbixMetric, ZabbixSender
 
 def get_conf():
     from ConfigParser import ConfigParser
@@ -11,13 +11,13 @@ def get_conf():
     conf['api'] = config.get('Telegram','api')
     conf['group_id'] = int(config.get('Telegram','group_id'))
     conf['telegram_enable'] = config.get('Telegram','enable')
-    conf['username'] = config.get('Zabbix','username')
-    conf['password'] = config.get('Zabbix','password')
-    conf['server'] = config.get('Zabbix','server')
+    conf['agentd_config'] = config.get('Zabbix','agentd_config')
     conf['zabbix_enable'] = config.get('Zabbix','enable')
     conf['apache_log'] = config.get('General','apache_log')
     conf['rules'] = config.get('General','rules')
     conf['apache_mask'] = config.get('General','apache_mask')
+    conf['vhost_enable'] = config.get('General','vhost_enable')
+
     return conf
 
 def get_file_rules():
@@ -25,7 +25,6 @@ def get_file_rules():
     with open('etc/default_filter.json','w') as file_rules:
         file_rules.write(r.content)
         file_rules.close()
-
 
 def owasp(path):
     for filtro in rules['filters']['filter']:
@@ -35,13 +34,24 @@ def owasp(path):
         except:
             pass
 
+def send_zabbix(msg):
+    metrics = [ZabbixMetric('Zabbix server', 'artlas_check', 'OK')]
+    ZabbixSender(use_config=conf['agentd_config']).send(metrics)
+    metrics = [ZabbixMetric('Zabbix server', 'artlas_check', msg)]
+    ZabbixSender(use_config=conf['agentd_config']).send(metrics)
+
 def connections(linha):
     line_parser = apache_log_parser.make_parser(conf['apache_mask'])
     log = line_parser(linha)
+    if conf['vhost_enable'] == 'True':
+        log['vhost'] = linha.split(' ')[0]
+    else:
+        log['vhost'] = None
     log['owasp'] = owasp(log['request_url'])
     if log['owasp']:
         msg ='''[+] - Intrusion Attempt - [+]
 Date: %s
+Vhost: %s
 IP: %s
 Path: %s
 User-Agent: %s
@@ -49,12 +59,15 @@ Browser: %s
 S.O: %s
 Description: %s
 Impact: %s
-Category: %s''' %(log['time_received'],log['remote_host'],log['request_url'],
+Category: %s''' %(log['time_received'],log['vhost'],log['remote_host'],log['request_url'],
         log['request_header_user_agent'],log['request_header_user_agent__browser__family']+' '+log['request_header_user_agent__browser__version_string'],
         log['request_header_user_agent__os__family'],log['owasp']['description'],
         log['owasp']['impact'],','.join(log['owasp']['tags']['tag']))
         print msg
         print
+        if conf['zabbix_enable'] == 'True':
+            send_zabbix(msg)
+
         if conf['telegram_enable'] == 'True':
             time.sleep(3)
             bot.sendMessage(conf['group_id'], msg)
@@ -72,13 +85,6 @@ print 'A.R.T.L.A.S Started!\n'
 #Check Telegram enabled
 if conf['telegram_enable'] == 'True':
     bot = telepot.Bot(conf['api'])
-    print '[+] - Telegram Enabled'
-
-#Check Zabbix enabled
-if conf['zabbix_enable'] == 'True':
-    zapi = ZabbixAPI(server)
-    zapi.login(conf['username'], conf['password'])
-    print '[+] - Zabbix Enabled'
 
 rules = json.loads(open(conf['rules']).read())
 
